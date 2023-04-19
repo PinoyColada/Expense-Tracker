2be169baa843a2d6003f57b2874508001b905855
pinoycolada <pinoydol.colada@gmail.com>
2023-04-19 18:03:26 -0400

    modified expenseitem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div>
+            <div>April 19th 2023</div>
+            <h2>Car Insurance</h2>
+            <div>$294.67</div>
+        </div>
+
+    )
 }
 
 export default ExpenseItem;
