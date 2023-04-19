9b4290c371e0e711851e8093344a13809610a3bc
pinoycolada <pinoydol.colada@gmail.com>
2023-04-19 18:11:37 -0400

    added styling to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>April 19th 2023</div>
+            <div className="expense-item__description">
             <h2>Car Insurance</h2>
-            <div>$294.67</div>
+            <div className="expense-item__price">$294.67</div>
+            </div>
         </div>
 
     )
